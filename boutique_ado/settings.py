971a08031ba0a48e9682b8b3493201921fa4b22f
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-09-17 21:37:36 +0100

    Add basic checkout functionality

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -177,5 +177,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # where to store uploaded files
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
