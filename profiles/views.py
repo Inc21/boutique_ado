0f175e11f0ecc3afc001565b333ac8a82a7e2970
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-09-23 00:38:35 +0100

    Add profile form, views and update template

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
 from .forms import UserForm
+from django.contrib import messages
 
 
 def profile(request):
     """ Display the user's profile. """
 
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
     form = UserForm(instance=profile)
     orders = profile.orders.all()
 
@@ -14,6 +22,7 @@ def profile(request):
     context = {
         'form': form,
         'orders': orders,
+        'on_profile_page': True
         }
 
     return render(request, template, context)
