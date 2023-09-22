a4d648dc5cbf3f2420282f1bfc202ceddf22dee8
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-09-22 01:23:03 +0100

    Add profile page and style profile form

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
+from .forms import UserForm
 
 
 def profile(request):
     """ Display the user's profile. """
 
     profile = get_object_or_404(UserProfile, user=request.user)
+    form = UserForm(instance=profile)
+    orders = profile.orders.all()
 
     template = 'profiles/profile.html'
-    context = {'profile': profile}
+    context = {
+        'form': form,
+        'orders': orders,
+        }
 
     return render(request, template, context)
