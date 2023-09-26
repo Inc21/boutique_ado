62172e78364fd5cbabe0c3778a8aef1d5d4e2ba5
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-09-26 22:13:15 +0100

    Add functionality to send real emails

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -221,7 +221,17 @@ if 'USE_AWS' in os.environ:
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+    DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
